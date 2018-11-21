5efba91fbe757a2efdda9d3c8bbdee33134fff5a
M.G.A <mazen.alhrishy@gmail.com>
2018-11-21 22:49:23 +0000

    - Microsoft SQL connection string as arguments example

---
+++ connect-manage-database/cnxn2.py
@@ -0,0 +1,4 @@
+cnxn = pyodbc.connect(driver='{SQL Server}',
+                      server='serverName',
+                      database='databaseName',
+                      trusted_connection='yes')

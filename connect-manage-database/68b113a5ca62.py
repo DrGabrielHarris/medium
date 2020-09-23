ddb3a71f7f11245c99008ece2294f37a92616ad5
Gabriel Harris <mazen.alhrishy@nielsen.com>
2020-09-23 13:29:27 +0100

    Move pandas to new article

--- connect-manage-database/68b113a5ca62.py
+++ connect-manage-database/68b113a5ca62.py
@@ -1,26 +1,33 @@
-import pyodbc
-import sqlite3
+import os
 import textwrap
-import pandas as pd
 
-# using PyODBC connection
-cnxn = pyodbc.connect('driver={SQL Server};'
-                      'server=serverName;'
-                      'database=databaseName;'
-                      'trusted_consnection=yes')
+import pyodbc
 
-# using sqlite3 connection
-# cnxn = sqlite3.connect('database.db')
+user = 'UserID'
+password = 'password'
+host = 'serverName'
+database = 'databaseName'
+driver = 'SQL Server'
 
-sql_query_string = textwrap.dedent("""
-SELECT customer.lname,
-       customer.fname,
-       address.city,
-       address.code
-FROM   T_CUSTOMERS AS customer 
-       LEFT JOIN T_ADDRESSES AS address ON address.id = customer.id 
-WHERE  customer.lname = 'Trujillo' 
-""")
+try:
+    user = os.environ['SQL_USERNAME']
+    password = os.environ['SQL_PASSWORD']
+except KeyError:
+    raise KeyError('SQL_USERNAME and/or SQL_PASSWORD env variable not found')
 
-# reading the extracted data into a DataFrame
-df = pd.read_sql(sql_query_string, cnxn)
+cnxn = pyodbc.connect(
+    driver=driver,
+    server=host,
+    database=database,
+    uid=user,
+    pwd=password,
+)
+
+cursor = cnxn.cursor()
+
+with open('get_customer_details.sql', 'r') as f:
+    sql_query_string = textwrap.dedent(f.read())
+
+rows = cursor.execute(sql_query_string).fetchall()
+
+cnxn.close()

e39c54371f7fcc57e7dfed5e51b87a84b392541e
Gabriel Harris Ph.D <dr.gabriel.harris@gmail.com>
2022-01-01 21:18:41 +0000

    Move from Medium repo to own repo
    https://github.com/DrGabrielHarris/connect-manage-database.git

--- connect-manage-database/get_customer_details.sql
+++
@@ -1,7 +0,0 @@
-SELECT customer.lname,
-          customer.fname,
-          address.city,
-          address.code
-FROM   T_CUSTOMERS AS customer 
-          LEFT JOIN T_ADDRESSES AS address ON address.id = customer.id 
-WHERE  customer.lname = 'Trujillo'
